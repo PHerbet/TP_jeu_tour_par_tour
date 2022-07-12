526f2f005dc4e6e6bf20c473727a37900542f4c9
Lethiers <fabien.tarne@gmail.com>
2022-07-12 14:33:54 +0200

    bouton attack fin de jeu

--- app.js
+++ app.js
@@ -8,8 +8,34 @@ const app = Vue.createApp({
         };
     },
     methods: {
+
+        attackPlayer(){
+            this.atk = 10;
+            this.pvIa -= this.atk;
+            console.log("l'attaque vient d'enlever 10pv");
+
+            test = Math.floor(Math.random()*(20-10)+10);
+            this.atk= test;
+            this.pvPlayer -= this.atk;
+            console.log(`l'IA vient de vous enlever ${test} pv`)  
+        },
+        
     },
     computed: {
+
+    },
+
+    watch: {
+        pvIa(value){
+            if (value <= 0) {
+                console.log('tu as gagné')
+            }
+        },
+        pvPlayer(value){
+            if (value <= 0) {
+                console.log('tu as perdu')
+            }
+        }
 
     }
     });
