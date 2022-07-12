7033ad8fecfdc2ef3fee692cd289ee4f47d99c5c
Priscillien herbet <priscillien.herbet@gmail.com>
2022-07-12 14:34:43 +0200

    add flee button fonctions

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const app = Vue.createApp({
             pvIa : 100,
             tour : 0,
             atk : 0,
+            fuite: true,
+            game_over: true,
         };
     },
     methods: {
+        flee(x) {
+            if (x === 1) {
+                this.fuite = !this.fuite;
+            }
+        if (x === 2){
+            this.game_over = false;
+        }
+        }
     },
     computed: {
 
