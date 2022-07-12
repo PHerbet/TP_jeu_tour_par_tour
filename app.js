9c313379406502f5c466ffbea8713623ec3eac08
Anthony <anthony.gyselman@gmail.com>
2022-07-12 23:04:48 +0200

    ff7 projet

--- app.js
+++ app.js
@@ -5,45 +5,59 @@ const app = Vue.createApp({
             pvIa : 100,
             tour : 0,
             atk : 0,
-            test:false
+            test:false,
+            combat:'',
+            combatResponse: [],
+            width: 0
         };
     },
     
     methods: {
         attackPlayer(){
+
+            if (this.combatResponse.length > 2) {
+                this.combatResponse = [];
+            }
+
             this.atk = 10;
             this.pvIa -= this.atk;
-            console.log("l'attaque vient d'enlever 10pv");
+            // console.log(`l'attaque vient d'enlever ${this.atk} PV à l'énemie`);
+            this.combat = `l'attaque vient d'enlever ${this.atk} PV à l'énemie`;
+            this.combatResponse.push(this.combat);
 
-            test = Math.floor(Math.random()*(20-10)+10);
-            this.atk= test;
+            random = Math.floor(Math.random()*(20-10)+10);
+            this.atk = random;
             this.pvPlayer -= this.atk;
-            console.log(`l'IA vient de vous enlever ${test} pv`)
-
+            // console.log(`l'IA vient de vous enlever ${random} PV`);
+            this.combat = `l'IA vient de vous enlever ${random} PV`;
+            this.combatResponse.push(this.combat);
+            // console.log(this.combatResponse);
             // incrementation tour
             if (this.tour === 3) {
-                this.tour =0;
+                this.tour = 0;
                 
-            }else{
+            } else {
                 this.tour++;  
-                console.log(`this.tour = ${this.tour}`);
+                // console.log(`tour = ${this.tour}`);
             }
+
+            
+
 
         },
 
         attackSpecial(){
             this.atk = 20;
             this.pvIa -= this.atk;
-            console.log("l'attaque spé enlève 20pv");
+            console.log(`l'attaque spé enlève ${this.atk} PV`);
 
-            test = Math.trunc(Math.random()*(20-10)+10);
-            this.atk= test;
+            random = Math.trunc(Math.random()*(20-10)+10);
+            this.atk = random;
             this.pvPlayer -= this.atk;
-            console.log(`l'IA vient de vous enlever ${test}`);
+            console.log(`l'IA vient de vous enlever ${random}`);
             this.test = !this.test;
-            this.tour =0;
-
-            console.log(this.test);
+            this.tour = 0;
+            // console.log(this.test);
             }
         },
     computed: {
@@ -55,7 +69,8 @@ const app = Vue.createApp({
                 this.test = !this.test;
 
             }
-        }
+        },
+
     }
     
     });
