b57caf45be49759a4502ce4d0e821973aafcf0a5
Anthony Gyselman <anthony.gyselman@gmail.com>
2022-07-12 15:19:34 +0200

    anthony

--- app.js
+++ app.js
@@ -5,12 +5,58 @@ const app = Vue.createApp({
             pvIa : 100,
             tour : 0,
             atk : 0,
+            test:false
         };
     },
+    
     methods: {
-    },
+        attackPlayer(){
+            this.atk = 10;
+            this.pvIa -= this.atk;
+            console.log("l'attaque vient d'enlever 10pv");
+
+            test = Math.floor(Math.random()*(20-10)+10);
+            this.atk= test;
+            this.pvPlayer -= this.atk;
+            console.log(`l'IA vient de vous enlever ${test} pv`)
+
+            // incrementation tour
+            if (this.tour === 3) {
+                this.tour =0;
+                
+            }else{
+                this.tour++;  
+                console.log(`this.tour = ${this.tour}`);
+            }
+
+        },
+
+        attackSpecial(){
+            this.atk = 20;
+            this.pvIa -= this.atk;
+            console.log("l'attaque spé enlève 20pv");
+
+            test = Math.trunc(Math.random()*(20-10)+10);
+            this.atk= test;
+            this.pvPlayer -= this.atk;
+            console.log(`l'IA vient de vous enlever ${test}`);
+            this.test = !this.test;
+            this.tour =0;
+
+            console.log(this.test);
+            }
+        },
     computed: {
 
+    },
+    watch: {
+        tour(value){
+            if(value === 3){
+                this.test = !this.test;
+
+            }
+        }
     }
+    
     });
     app.mount('#monApp');
