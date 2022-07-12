f80b15057fcd9f7045fe60bfc147e9ddb28062b1
Priscillien herbet <priscillien.herbet@gmail.com>
2022-07-12 12:21:58 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            pvPlayer : 100,
+            pvIa : 100,
+            tour : 0,
+            atk : 0,
+        };
+    },
+    methods: {
+    },
+    computed: {
+
+    }
+    });
+    app.mount('#monApp');
