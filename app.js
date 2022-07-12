2976959d90d8fe70cb6026af5a64bca110625fb1
black <madimad@gmail.com>
2022-07-12 14:39:22 +0200

    add function heal

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const app = Vue.createApp({
     data() {
         return {
-            pvPlayer : 100,
+            pvPlayer : 50,
             pvIa : 100,
             tour : 0,
             atk : 0,
         };
     },
     methods: {
+heal() {
+            if(this.pvPlayer > 0 && this.pvPlayer < 100){
+                this.pvPlayer = this.pvPlayer +20;
+                this.tour = this.tour +1;
+            }
+            
+            if(this.pvPlayer > 100){
+                this.pvPlayer=100;
+            }
+            console.log("+20 de pv pour le Player");
+        }
     },
-    computed: {
+    computed:{
+        
 
     }
     });
