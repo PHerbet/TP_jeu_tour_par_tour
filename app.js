923ad1e6229741968d215d9306a52c240691382f
Lethiers <fabien.tarne@gmail.com>
2022-07-13 11:08:12 +0200

    correction bug il faut remplacer les messages d'alert

--- app.js
+++ app.js
@@ -17,14 +17,73 @@ const app = Vue.createApp({
     },
     
     methods: {
-        flee(x) {
-            if (x === 1) {
-                this.fuite = !this.fuite;
+
+        attackPlayer(){
+            //gestion des messages de combat
+            if (this.combatResponse.length > 2) {
+                this.combatResponse = [];
             }
-        if (x === 2){
-            this.game_over = false;
-        }
+            //gestion des messages de combat
+            //////////////////// PV perdu //////////////
+            this.atk = 10;
+            this.pvIa -= this.atk;
+            //////////////////// PV perdu //////////////
+            this.combat = `l'attaque vient d'enlever ${this.atk} PV à l'énnemi`;
+            this.combatResponse.push(this.combat);
+            //gestion des messages de combat
+
+            // ataque IA
+            random = Math.floor(Math.random()*(20-10)+10);
+            this.atk = random;
+            this.pvPlayer -= this.atk;
+            this.combat = `l'IA vient de vous enlever ${random} PV`;
+            this.combatResponse.push(this.combat);
+             // ataque IA
+
+            // incrementation tour 
+            if (this.tour === 3) {
+                this.tour = 0;    
+            } else {
+                this.tour++;  
+                console.log(`tour = ${this.tour}`);
+            }
+            // incrementation tour 
         },
+
+        attackSpecial(){
+            if (this.combatResponse.length > 2) {
+                this.combatResponse = [];
+            }
+            //gestion des messages de combat
+
+            //////////////////// PV perdu //////////////
+            this.atk = 20;
+            this.pvIa -= this.atk;
+            //////////////////// PV perdu //////////////
+            //gestion des messages de combat
+            this.combat = `l'attaque spéciale d'enlever ${this.atk} PV à l'énnemi`;
+            this.combatResponse.push(this.combat);
+            //gestion des messages de combat
+
+        
+            console.log(`l'attaque spé enlève ${this.atk} PV`);
+
+            // gestion de l'IA
+            random = Math.trunc(Math.random()*(20-10)+10);
+            this.atk = random;
+            this.pvPlayer -= this.atk;
+
+            //gestion des messages de combat
+            this.combat = `l'IA vient de vous enlever ${random}`;
+            this.combatResponse.push(this.combat);
+            //gestion des messages de combat
+
+
+
+            this.test = !this.test;
+            this.tour = 0;
+            
+            },
 
     heal() {
             if(this.pvPlayer > 0 && this.pvPlayer < 100){
@@ -36,58 +95,52 @@ const app = Vue.createApp({
             if(this.pvPlayer > 100){
                 this.pvPlayer=100;
             }
-            this.combat = `vous regagnez 20 pv`;
+           
+
+            //gestion des messages de combat
+            this.combat = `vous avez maintenant ${this.pvPlayer}pv`;
+            this.combatResponse.push(this.combat);
+            //gestion des messages de combat
+
+            // gestion de l'IA
+            random = Math.trunc(Math.random()*(20-10)+10);
+            this.atk = random;
+            this.pvPlayer -= this.atk;
+            
+            //gestion des messages de combat
+            this.combat = `l'IA vient de vous enlever ${random}`;
+            this.combatResponse.push(this.combat);
+           //gestion des messages de combat
+        },
+        flee(x) {
+            if (x === 1) {
+                this.fuite = !this.fuite;
+            }
+        if (x === 2){
+            this.game_over = false;
+        }
         }
     },
     computed:{
         
-    attackPlayer(){
-            if (this.combatResponse.length > 2) {
-                this.combatResponse = [];
-            }
-
-            this.atk = 10;
-            this.pvIa -= this.atk;
-            // console.log(`l'attaque vient d'enlever ${this.atk} PV à l'énnemi`);
-            this.combat = `l'attaque vient d'enlever ${this.atk} PV à l'énnemi`;
-            this.combatResponse.push(this.combat);
-
-            random = Math.floor(Math.random()*(20-10)+10);
-            this.atk = random;
-            this.pvPlayer -= this.atk;
-            // console.log(`l'IA vient de vous enlever ${random} PV`);
-            this.combat = `l'IA vient de vous enlever ${random} PV`;
-            this.combatResponse.push(this.combat);
-            // console.log(this.combatResponse);
-            // incrementation tour
-            if (this.tour === 3) {
-                this.tour = 0;
-                
-            } else {
-                this.tour++;  
-                console.log(`tour = ${this.tour}`);
-            }
-        },
-
-        attackSpecial(){
-            this.atk = 20;
-            this.pvIa -= this.atk;
-            console.log(`l'attaque spé enlève ${this.atk} PV`);
-
-            random = Math.trunc(Math.random()*(20-10)+10);
-            this.atk = random;
-            this.pvPlayer -= this.atk;
-            console.log(`l'IA vient de vous enlever ${random}`);
-            this.test = !this.test;
-            this.tour = 0;
-            // console.log(this.test);
-            }
         },
     
     watch: {
         tour(value){
             if(value === 3){
                 this.test = !this.test;
+            }
+        },
+        pvIa(value){
+            if (value <= 0 && this.pvPlayer > 0) {
+                alert('tu as gagné');
+                this.pvIa = 0;
+            }
+        },
+        pvPlayer(value){
+            if (value <= 0) {
+                alert('tu as perdu');
+                this.pvPlayer = 0;
             }
         },
     },
